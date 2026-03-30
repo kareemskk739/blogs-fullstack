76a1b97492cbc03da411b49554f8f9628e4b4e19
kareemskk739 <skkareem739@gmail.com>
2026-03-30 17:07:57 +0530

    created super user

---
+++ backend/api/management/commands/createsu.py
@@ -0,0 +1,22 @@
+from django.core.management.base import BaseCommand
+from django.contrib.auth import get_user_model
+
+class Command(BaseCommand):
+    help = "Create superuser automatically"
+
+    def handle(self, *args, **kwargs):
+        User = get_user_model()
+
+        username = "admin"
+        email = "admin@gmail.com"
+        password = "admin123"
+
+        if not User.objects.filter(username=username).exists():
+            User.objects.create_superuser(
+                username=username,
+                email=email,
+                password=password
+            )
+            self.stdout.write(self.style.SUCCESS("Superuser created"))
+        else:
+            self.stdout.write(self.style.WARNING("Superuser already exists"))
